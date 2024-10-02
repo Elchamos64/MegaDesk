15aef8b3f9445b9bfbc185a18d3a85484bc2520e
Oscar Ramos <131794552+Elchamos64@users.noreply.github.com>
2024-10-02 13:51:04 -0600

    Add project files.

---
+++ MegaDesk/MainMenu.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace MegaDesk
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addQuoteBtn = new System.Windows.Forms.Button();
+            this.viewQuotesBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.searchQuotesBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // addQuoteBtn
+            // 
+            this.addQuoteBtn.AutoSize = true;
+            this.addQuoteBtn.Location = new System.Drawing.Point(173, 84);
+            this.addQuoteBtn.Name = "addQuoteBtn";
+            this.addQuoteBtn.Size = new System.Drawing.Size(131, 30);
+            this.addQuoteBtn.TabIndex = 0;
+            this.addQuoteBtn.Text = "Add New Quote";
+            this.addQuoteBtn.UseVisualStyleBackColor = true;
+            // 
+            // viewQuotesBtn
+            // 
+            this.viewQuotesBtn.AutoSize = true;
+            this.viewQuotesBtn.Location = new System.Drawing.Point(173, 132);
+            this.viewQuotesBtn.Name = "viewQuotesBtn";
+            this.viewQuotesBtn.Size = new System.Drawing.Size(109, 30);
+            this.viewQuotesBtn.TabIndex = 1;
+            this.viewQuotesBtn.Text = "View Quotes";
+            this.viewQuotesBtn.UseVisualStyleBackColor = true;
+            this.viewQuotesBtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.AutoSize = true;
+            this.exitBtn.Location = new System.Drawing.Point(173, 240);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(109, 30);
+            this.exitBtn.TabIndex = 2;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            // 
+            // searchQuotesBtn
+            // 
+            this.searchQuotesBtn.AutoSize = true;
+            this.searchQuotesBtn.Location = new System.Drawing.Point(173, 188);
+            this.searchQuotesBtn.Name = "searchQuotesBtn";
+            this.searchQuotesBtn.Size = new System.Drawing.Size(126, 30);
+            this.searchQuotesBtn.TabIndex = 3;
+            this.searchQuotesBtn.Text = "Search Quotes";
+            this.searchQuotesBtn.UseVisualStyleBackColor = true;
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.searchQuotesBtn);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.viewQuotesBtn);
+            this.Controls.Add(this.addQuoteBtn);
+            this.Name = "MainMenu";
+            this.Text = "MegaDeskRamos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addQuoteBtn;
+        private System.Windows.Forms.Button viewQuotesBtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Button searchQuotesBtn;
+    }
+}
+
---
+++ MegaDesk/MainMenu.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MegaDesk
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void addQuoteBtn_Click(object sender, EventArgs e)
+        {
+            AddQuote addQuoteForm = new AddQuote();
+            addQuoteForm.Show();
+        }
+
+        private void viewQuotesBtn_Click(object sender, EventArgs e)
+        {
+            ViewAllQuotes viewAllQuotesForm = new ViewAllQuotes();
+            viewAllQuotesForm.Show();
+        }
+
+        private void searchQuotesBtn_Click(object sender, EventArgs e)
+        {
+            SearchQuotes searchQuotesForm = new SearchQuotes();
+            searchQuotesForm.Show();
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
